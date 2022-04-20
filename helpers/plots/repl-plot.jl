a96af16ed3eeec151de01b52cf4c490c8adba815
artniewski <artniewski@gmail.com>
2022-04-20 11:34:58 +0200

    Add Plotting

---
+++ helpers/plots/repl-plot.jl
@@ -0,0 +1,15 @@
+using Plots
+
+function myplot(road::Array{Int}, nodes::Array{Int}, connect::Bool)
+    plt = scatter(nodes[:, 1], nodes[:, 2], label=false)
+    xaxis = []
+    road .|> (x -> push!(xaxis, nodes[x, 1]))
+    yaxis = []
+    road .|> (x -> push!(yaxis, nodes[x, 2]))
+    if connect
+        push!(xaxis, xaxis[1])
+        push!(yaxis, yaxis[1])
+    end
+    plt = plot!(xaxis, yaxis, label=false)
+    display(plt)
+end
