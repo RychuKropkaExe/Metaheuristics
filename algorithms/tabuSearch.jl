c172bb00ad117322122a800b7b2a46fbea920639
artniewski <artniewski@gmail.com>
2022-04-28 19:49:49 +0200

    Add comments and aspiration

--- algorithms/tabuSearch.jl
+++ algorithms/tabuSearch.jl
@@ -33,45 +33,50 @@ function tabuSearch(
             if time_elapsed > TIME_LIMIT || (iterStop && iterations > iterNumber)
                 return bestCities
             end
-
+            #check time since last update 
             if upgrade_time_elapsed > UPGRADE_TIME
                 upgrade_time_elapsed = Second(0)
                 upgrade_start = Dates.now()
                 if isempty(longTermMemory)
                     shuffle!(globalCities)
-                    println("UPGRADING")
+                    println("UPGRADING (shuffle)")
                 else
                     #kick = rand(1:length(longTermMemory))
                     kick = pop!(longTermMemory)
                     globalCities = kick[1]
                     tabuList = kick[2]
                     localCities = kick[1]
-                    println(tabuList)
+                    #println(tabuList)
                     println("KICKING")
                     sleep(5)
                 end
                 break
             end
 
-            if [i, j] ∈ tabuList || [j, i] ∈ tabuList
-                continue
-            end
-
+            # Copy localBest and invert
             currCities::Array{Int} = copy(localCities)
             part = view(currCities, i:j)
             reverse!(part)
-
             currDist::Float64 = destination(graph, currCities)
+            #Update time
             time_elapsed = Dates.now() - start
             upgrade_time_elapsed = Dates.now() - upgrade_start
-
+            #Check if on tabu list
+            if [i, j] ∈ tabuList || [j, i] ∈ tabuList
+                if (currDist >= bestDist)
+                    continue
+                end
+            end
+            #Check if better than localBest
             if currDist < localDist
                 localCities = currCities
                 localDist = currDist
                 move = [i, j]
             end
-        end
+        end #end of for loops
+        #set globalBest as last localBest
         globalCities = localCities
+        #update All best result if local/global is better
         if localDist < bestDist
             bestDist = localDist
             bestCities = localCities
@@ -83,9 +88,11 @@ function tabuSearch(
             push!(savedTabu, move)
             push!(longTermMemory, [globalCities, savedTabu])
             println("New best distance: ", bestDist)
+            #update time since last update
             upgrade_time_elapsed = Second(0)
             upgrade_start = Dates.now()
         end
+        #add move to tabuList
         if move != [-1, -1]
             popfirst!(tabuList)
             push!(tabuList, move)
@@ -94,3 +101,5 @@ function tabuSearch(
     end
     return bestCities
 end
+#TODO: stagnacja zamiana czasu na liczbe iteracji i zwiekszenie tego wraz ze wszystkimi iteracjami
+#TODO: dodać aspiracje
--- tests/main.jl
+++ tests/main.jl
@@ -4,22 +4,32 @@ using TSPLIB
 include("../utils/all.jl")
 include("../algorithms/all.jl")
 
-function xd()
+function tabuThreads(weights::Matrix, dimension)
+    results = []
+    bestDist = typemax(Float64)
+    lk = ReentrantLock()
     Threads.@threads for i in 1:Threads.nthreads()
+        list = tabuSearch(kRandom(weights, dimension, 1000), weights, 60, floor(Int, sqrt(dimension)), 10, false, 4000)
+        lock(lk)
+        if (destination(weights, list) < bestDist)
+            results = list
+        end
+        unlock(lk)
     end
+    return results
 end
 
 function main()
-    dict = structToDict(readTSPLIB(:berlin52))
+    dict = structToDict(readTSPLIB(:a280))
     dimension = dict[:dimension]
     weights = dict[:weights]
     nodes = dict[:nodes]
     println(dict[:optimal])
 
     #list = tabuSearch(twooptacc(kRandom(weights,dimension,1000),weights,nodes, false), weights,300,floor(Int,sqrt(dimension)))
-    list = tabuSearch(kRandom(weights, dimension, 1000), weights, 300, floor(Int, sqrt(dimension)), 10, false, 4000)
-    println(destination(weights, list))
-    # destination(weights, list) |> println
+    #list = tabuThreads(weights, dimension)
+    list = tabuSearch(kRandom(weights, dimension, 1000), weights, 3000, floor(Int, sqrt(dimension)), 10, false, 4000)
+    destination(weights, list) |> println
     # sleep(3)
     # #list::Array{Int} = nearestforall(weights, dimension, nodes, false)
     # #destination(weights, list) |> println
