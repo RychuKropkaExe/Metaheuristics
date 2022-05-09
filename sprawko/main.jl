364c4f505497b52bbbbe0f6a2d4fb50dbe17e83d
RychuKropkaExe <juzek9898@o2.pl>
2022-05-09 23:54:39 +0200

    Added parameter tuning

--- sprawko/main.jl
+++ sprawko/main.jl
@@ -1,10 +1,8 @@
 include("utils.jl")
-function swa(a::Array,i,j)
-    a[i],a[j] = a[j],a[i]
-end
-function main(func::Function)
-    a = [1,2,3,4,5,6,7,8]
-    func(a,2,7)
-    println(a)
-end
-main(swa)
+
+
+
+
+
+
+tuning()
--- sprawko/tests.jl
+++ sprawko/tests.jl
@@ -1,4 +1,22 @@
+include("../algorithms/all.jl")
 using TSPLIB
+using Hyperopt
+using Plots
 
 
-
+function tuning()
+    dict = structToDict(readTSPLIB(:d1291))
+    dimension = dict[:dimension]
+    weights = dict[:weights]
+    nodes = dict[:nodes]
+    println(dict[:optimal])
+    base = kRandom(weights,dimension,1)
+    arr = Array{Int,1}(1:dimension)
+    arr2 = Array{Int,1}(20:floor(Int,log(dimension)):dimension)
+    append!(arr,arr2)
+    #b = Array{Int,1}(1:floor(log(dimension)))
+    @time (ho = @hyperopt for i = 100, a = arr, b = 1
+        cost = destination(weights,nearest3(weights,dimension,[a]))
+    end)
+    println(ho.minimizer, ":",ho.minimum, ":", ho.maximizer, ":", ho.maximum)
+end
