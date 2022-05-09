2d9a279664087edbbda954e8acf47d85df2829d1
RychuKropkaExe <juzek9898@o2.pl>
2022-05-10 00:15:38 +0200

    adding test files

--- sprawko/tests.jl
+++ sprawko/tests.jl
@@ -5,18 +5,29 @@ using Plots
 
 
 function tuning()
-    dict = structToDict(readTSPLIB(:d1291))
-    dimension = dict[:dimension]
-    weights = dict[:weights]
-    nodes = dict[:nodes]
-    println(dict[:optimal])
-    base = kRandom(weights,dimension,1)
-    arr = Array{Int,1}(1:dimension)
-    arr2 = Array{Int,1}(20:floor(Int,log(dimension)):dimension)
-    append!(arr,arr2)
-    #b = Array{Int,1}(1:floor(log(dimension)))
-    @time (ho = @hyperopt for i = 100, a = arr, b = 1
-        cost = destination(weights,nearest3(weights,dimension,[a]))
-    end)
-    println(ho.minimizer, ":",ho.minimum, ":", ho.maximizer, ":", ho.maximum)
+    arrx = []
+    arry = []
+    for (root, dirs, files) in walkdir("./plikiTSP")
+        for file in files
+            path = "./plikiTSP/"*file
+            dict = struct_to_dict(readTSP(path))
+            graph = dict[:weights]
+            dimension = dict[:dimension]
+            dict = struct_to_dict(readTSP(file))
+            base = kRandom(weights,dimension,1)
+            arr = Array{Int,1}(1:floor(Int,log(dimension)))
+            arr2 = Array{Int,1}(floor(Int,log(dimension)):floor(Int,log(dimension)):dimension)
+            append!(arr,arr2)
+            #b = Array{Int,1}(1:floor(log(dimension)))
+            ho = @phyperopt for i = 100, a = arr, b = 1
+                cost = destination(weights,kRandom(weights,dimension,a))
+            end
+            println(ho.minimizer, ":",ho.minimum, ":", ho.maximizer, ":", ho.maximum)
+            push!(arrx,file)
+            push!(arry, ho.minimizer[1])
+            i+=1
+        end
+    end
+    println(arrx)
+    println(arry)
 end
