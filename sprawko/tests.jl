dbe3b161995add375d9f09af1af6021a6de308eb
RychuKropkaExe <juzek9898@o2.pl>
2022-05-10 00:51:13 +0200

    Finishing seting up parameter tner, for mass test

--- sprawko/tests.jl
+++ sprawko/tests.jl
@@ -10,20 +10,17 @@ function tuning()
     arry = []
     for (root, dirs, files) in walkdir("./plikiTSP")
         for file in files
-            println(pwd())
-            println(file)
-            path = root*"/"*file
+            path = "./plikiTSP/"*file
             dict = structToDict(readTSP(path))
             graph = dict[:weights]
             dimension = dict[:dimension]
-            dict = struct_to_dict(readTSP(file))
-            base = kRandom(weights,dimension,1)
+            base = kRandom(graph,dimension,1)
             arr = Array{Int,1}(1:floor(Int,log(dimension)))
             arr2 = Array{Int,1}(floor(Int,log(dimension)):floor(Int,log(dimension)):dimension)
             append!(arr,arr2)
             #b = Array{Int,1}(1:floor(log(dimension)))
             ho = @phyperopt for i = 100, a = arr, b = 1
-                cost = destination(weights,kRandom(weights,dimension,a))
+                cost = destination(graph,kRandom(graph,dimension,a))
             end
             println(ho.minimizer, ":",ho.minimum, ":", ho.maximizer, ":", ho.maximum)
             push!(arrx,file)
