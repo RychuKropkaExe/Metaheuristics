0d3a09c6b9e2914ae943e5266d8267a985a8ebe5
artniewski <artniewski@gmail.com>
2022-05-11 02:40:16 +0200

    Plotting script and results

---
+++ jsons/plots.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_json('/Users/artniewski/julia/traveling-salesman-problem/jsons/results-kox.json', orient='records')
+for tsp_problem in df:
+    for result in df[tsp_problem]:
+        kox = dict(sorted(result.items(), key=lambda key: int(key[0])))
+        keys = list(kox.keys())
+        values = list(kox.values())
+        # print(keys)
+        # print(values)
+        plt.plot(keys, values)
+        # plt.yscale('log')
+        # plt.minorticks_off()
+    plt.title(tsp_problem)
+    plt.show()
+    plt.clf()
