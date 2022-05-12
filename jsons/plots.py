e402122342d82eabcd45159a427de60bf59b4f60
artniewski <artniewski@gmail.com>
2022-05-12 13:39:50 +0200

    Add plots

--- jsons/plots.py
+++ jsons/plots.py
@@ -3,38 +3,44 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from collections import defaultdict
 import statistics as st
+import pathlib
 
 
 def main():
-    df = pd.read_json('results-long0.json', orient='records')
-    all_avg_dict = dict()
-    for tsp_problem, tsp_list in df.items():
-        list_of_dictionaries = list(tsp_list)
-        dd = defaultdict(list)
-        avg_dict = dict()
-        for d in list_of_dictionaries:
-            for k, v in d.items():
-                dd[k].append(v)
-        for k, v in dd.items():
-            avg_dict[int(k)] = int(np.floor(st.mean(v)))
-        all_avg_dict[tsp_problem] = avg_dict
-    print(all_avg_dict)
+    for result_type in ["long0", "rand_tabu", "reverse", "swap"]:
+        df = pd.read_json("results-" + result_type + ".json", orient='records')
+        all_avg_dict = dict()
+        for tsp_problem, tsp_list in df.items():
+            list_of_dictionaries = list(tsp_list)
+            dd = defaultdict(list)
+            avg_dict = dict()
+            for d in list_of_dictionaries:
+                for k, v in d.items():
+                    if k != '0':
+                        dd[k].append(v)
+            for k, v in dd.items():
+                avg_dict[int(k)] = int(np.floor(st.mean(v)))
+            all_avg_dict[tsp_problem] = avg_dict
 
-    for tsp_problem, tsp_list in df.items():
-        for d in tsp_list:
-            keys = [int(i) for i in d.keys()]
-            keys = np.sort(keys)
-            values = [d[str(i)] for i in keys]
+        for tsp_problem, tsp_list in df.items():
+            path = pathlib.Path("plots/" + tsp_problem)
+            path.mkdir(parents=True, exist_ok=True)
 
-            plt.yscale('log')
-            plt.plot(keys, values)
-        plt.title(tsp_problem)
-        curr_dict = all_avg_dict[tsp_problem]
-        keys = [int(i) for i in curr_dict.keys()]
-        values = [curr_dict[i] for i in keys]
-        plt.scatter(keys, values, c='black', zorder=10)
-        plt.show()
-        plt.clf()
+            for d in tsp_list:
+                keys = [int(i) for i in d.keys() if i != '0']
+                keys = np.sort(keys)
+                values = [d[str(i)] for i in keys]
+
+                plt.yscale('log')
+                plt.plot(keys, values)
+            plt.title(tsp_problem + " - " + result_type)
+            curr_dict = all_avg_dict[tsp_problem]
+            keys = [int(i) for i in curr_dict.keys()]
+            values = [curr_dict[i] for i in keys]
+            plt.scatter(keys, values, c='black', s=10, zorder=10)
+            plt.scatter(keys, values, c='white', s=1, zorder=10)
+            plt.savefig("plots/" + tsp_problem + "/" + result_type + ".png")
+            plt.clf()
 
 
 if __name__ == '__main__':
